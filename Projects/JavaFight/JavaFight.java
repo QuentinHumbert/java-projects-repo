825e6588c34863d23e0e4b1c78bca61e10fcc7ae
QuentinHumbert <quentinhum76@gmail.com>
2023-06-22 16:49:20 +0200

    First commit

---
+++ Projects/JavaFight/JavaFight.java
@@ -0,0 +1,29 @@
+public class JavaFight{
+    public static void main(String[] args){
+        // General attributes
+        boolean isCharacterHit;
+        int rangeDifference;
+
+        // Player attributes
+        String playerName = "Player";
+        String playerWeapon;
+        int playerHealth = 100;
+        int playerStamina = 50;
+        int playerWeaponDamage;
+        int playerWeaponRange;
+
+        // Enemy attributes
+        String enemyName = "Enemy";
+        String enemyWeapon;
+        int enemyHealth = 100;
+        int enemyStamina = 50;
+        int enemyWeaponDamage;
+        int enemyWeaponRange;
+        
+        // Weapon attributes
+        String weaponName;
+        int weaponDamage;
+        int weaponMinimumRange;
+        int weaponMaximumRange;
+    }
+}
